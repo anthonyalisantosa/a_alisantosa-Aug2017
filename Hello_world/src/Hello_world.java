70322222120daf5ba5cfcb1a98a76c0884fb8d6c
anthonyalisantosa <18alisantosaa46@millshighsch.org>
2017-08-18 09:39:54 -0700

    Hello_world program
    FeelsBadMan
    
    Instructions unclear.

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,14 @@
+
+/**
+ * FeelsBadMan
+ * 
+ * @author Anthony Alisantosa
+ * @version 2017.08.16
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Anthony Alisantosa"); //Display the string
+    }
+}
