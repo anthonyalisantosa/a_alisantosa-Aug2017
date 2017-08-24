7cc903af1f0f7dcb778a77d8581b3e6dc91fd6c3
anthonyalisantosa <18alisantosaa46@millshighsch.org>
2017-08-24 09:33:26 -0700

    Third time's the charm!

---
+++ Project_2A/src/Project_2A.java
@@ -0,0 +1,11 @@
+//Anthony Alisantosa
+//8/23/2017
+//Project 2 -- Use print and println
+public class Project_2A {
+
+	public static void main(String[] args) {
+		System.out.println("Hi there!");
+		System.out.print("Hi there!");
+	}
+		
+}
