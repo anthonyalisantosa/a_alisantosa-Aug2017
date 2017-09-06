5bff4f663f7e5579ecd0af7b4d4689fdfb487268
anthonyalisantosa <18alisantosaa46@millshighsch.org>
2017-09-06 10:03:53 -0700

    Thx Eric <3

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,8 @@
+//Anthony Alisantosa
+//2017 09 06
+//This is the runner for the calculate library. It is used to test the methods in Calculate
+public class DoMath {
+	public static void main(String[] args){
+		System.out.println(Calculate.square(5));
+	}
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,9 @@
+//Anthony Alisantosa
+//2017 09 06
+//Yellow sheet thing
+public class Calculate {
+	public static int square(int variable) {
+		return variable*variable;
+	}
+
+}
