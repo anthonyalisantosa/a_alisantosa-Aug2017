37aaf8b34252f29aa73a0a6d40b4d28161d308d7
anthonyalisantosa <18alisantosaa46@millshighsch.org>
2017-09-06 10:50:28 -0700

    Part Uno!

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -3,6 +3,17 @@
 //This is the runner for the calculate library. It is used to test the methods in Calculate
 public class DoMath {
 	public static void main(String[] args){
+
 		System.out.println(Calculate.square(5));
+		System.out.println(Calculate.cube(5));
+		System.out.println(Calculate.average(6, 17));
+		System.out.println(Calculate.average(5,7,12));
+		System.out.println(Calculate.toDegrees(3.14159));
+		System.out.println(Calculate.discriminant(4, 15, 8));
+		System.out.println(Calculate.toImproperFrac(4, 3, 5));
+		System.out.println(Calculate.toMixedNum(23,  5));
+
+	
 	}
+	
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -2,8 +2,48 @@
 //2017 09 06
 //Yellow sheet thing
 public class Calculate {
-	public static int square(int variable) {
-		return variable*variable;
+	
+	public static int square(int inputvariable) {
+	
+		return inputvariable*inputvariable;
 	}
+	
+	public static int cube(int inputvariable) {
+		
+		return inputvariable*inputvariable*inputvariable;
+	}
+	
+	public static double average(double inputvariable1, double inputvariable2) {
+			
+		return (inputvariable1 + inputvariable2) / 2;
+	}
+	
+	public static double average(double inputvariable1, double inputvariable2, double inputvariable3) {
+		
+		return (inputvariable1 + inputvariable2 + inputvariable3) / 3;
+	}
+	
+	public static double toDegrees(double inputvariable) {
+		
+		return ((inputvariable / (2*3.14159)) *360);	
+	}
+	
+	public static double discriminant(double input1,double input2, double input3) {
+		
+		return ((input2 * input2)- 4 * input1 * input2);
+	}
+	
+	public static String toImproperFrac(int wholenum, int numerator, int denominator) {
+		
+		return (wholenum * denominator) + numerator + "/" + denominator;
+	
+	}
+	
+	public static String toMixedNum(int numerator, int denominator) {
+		
+		return (numerator/denominator) + "_" + numerator%denominator + "/" + denominator;
+	}
+	
+
 
 }
