ce123bebcb257fceca2d9a27b7e066b628097b5d
anthonyalisantosa <18alisantosaa46@millshighsch.org>
2017-10-04 11:31:50 -0700

    Erichelp

--- CollaborativeProgrammingExercise/src/Programmer2scode.java
+++ CollaborativeProgrammingExercise/src/Programmer2scode.java
@@ -5,28 +5,17 @@ public class Programmer2scode {
 		System.out.println("Gib input plz");
 		Scanner input = new Scanner(System.in);
 		int userinput = input.nextInt();
-		System.out.println("Gib input plz");
-		int userinput2 = input.nextInt();
 		int min = userinput;
-		int max = 0;
-			if(userinput < userinput2) {
-				min = userinput;
-				int max = userinput2;
-			} else {
-				int min = userinput2;
-				int max = userinput;
-			}
-		System.out.println("Done? Y/N ");
-		String yesno = input.next();
-		if (yesno.equals("Y")){
-			System.out.println("max: " + max  );
-		}
+		int max = userinput;
+		int evensum = 0;
+		int evenmax = 1;
 		do {
 			System.out.println("Gib input plz");
 			int newuserinput = input.nextInt();
-				if (newuserinput > ) {
-				} else {
-					
+				if (newuserinput > max) {
+					max = newuserinput;
 				}
 		}
 	}
+}
+				
