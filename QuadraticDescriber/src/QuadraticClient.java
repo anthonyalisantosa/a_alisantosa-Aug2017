f354a5e2eb3a81d1c3be7de47d54daa234a0714c
anthonyalisantosa <18alisantosaa46@millshighsch.org>
2017-10-10 09:38:36 -0700

    Fixed to **completely** replicate code.
    -_-

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -5,25 +5,30 @@ import java.util.Scanner;
 //Part 5
 //2nd Period
 //Describing the quadratic equation for the client
+//
+//Takes the User's responses in the client. 
+//Prompts user through a series of Scanners.
 public class QuadraticClient {
 	public static void main(String[] args) {
 	//to make scanner Scanner <scannerName> = new Scanner();
 
-	Scanner clientinput = new Scanner(System.in);
-	String clientresponse = "";
-	do {
-	System.out.println("Please input your 3 coefficients:");
-	System.out.println("Enter coefficient a:");
+	Scanner clientinput = new Scanner(System.in);	//Creates new scanner to search for user input into the Console
+	String clientresponse = "";	//default value for string = empty
+	do {	//do/while loop (Does code while clientrespons.equals does not equal "quit")
+	System.out.println("Provide values for coefficients a, b, and c");
+	System.out.println("a:");
 	double a = clientinput.nextDouble();
-	System.out.println("Enter coefficient b:");
+	System.out.println("b:");
 	double b = clientinput.nextDouble();
-	System.out.println("Enter coefficient c:");
+	System.out.println("c:");
 	double c = clientinput.nextDouble();
 	System.out.println(); //A E S T H E T I C S  K E E P  I T  C L E A N
+	System.out.println("Description of the graph of:");
+	System.out.println(a + " x^2 " + b + " x " + c);
 	System.out.println(Quadratic.quadrDescriber(a, b, c));
-	System.out.println("Do you wish to continue? Y/N");
+	System.out.println("Do you want to keep going? (Type \"quit\" to end)");
 	clientresponse = clientinput.next();
-	} while(!clientresponse.equals("N"));
+	} while(!clientresponse.equals("quit"));
 		
 	}
 	
