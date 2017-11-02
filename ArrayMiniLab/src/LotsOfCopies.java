8eafb368bf9dd8dce1b77937d5f32dc94baa7673
anthonyalisantosa <31085589+anthonyalisantosa@users.noreply.github.com>
2017-11-02 16:14:46 -0700

    kd

--- ArrayMiniLab/src/LotsOfCopies.java
+++ ArrayMiniLab/src/LotsOfCopies.java
@@ -13,42 +13,84 @@ public class LotsOfCopies {
 		String strMain = "APCS";
 		int[] arrMain = {1, 2, 3, 4, 5};
 		
-		System.out.println("Nothing has changed... yet! Currently 'num' is " + num + "!");
-		System.out.println("Nothing has changed... yet! Currently 'strMain' is " + strMain + "!" );
-		System.out.println("Nothing has changed... yet! Currently  the fifth number of 'arrrMain'  is " + arrMain[4] + "!");
+		System.out.println("num is: " + num + " according to main before changeMe");
+		System.out.println("strMain is: \"" + strMain + "\" according to main before changeMe");
+		System.out.println("First number of the array is: " + arrMain[0] + " according to main before changeMe");
 		System.out.println();
+		/*
+		 * num is: 7 according to main before changeMe
+		 * strMain is: "APCS" according to main before changeMe
+		 * First number of the array is: 1 according to main before changeMe
+		 */
 		
 		changeMe(num, strMain, arrMain);
+		/*
+		 * num is: 8 according to changeMe
+		 * strMain is: "APCS period 2" according to changeMe
+		 * First number of the array is: 6 according to changeMe
+		 */
 		
-		System.out.println("Beep-boop. Method changeMe has changed num. num is now " + num + ". Beep boop.");
-		System.out.println("Beep-boop. Method changeMe has changed strMain. strMain is now \"" + strMain + "\". Blip blop.");
-		System.out.println("Beep-boop. Method changeMe has changed arrMain. The fifth number of arrMain is now " + arrMain[4] + ". Ding dong.");
-		
+		System.out.println("num is: " + num + " according to main after changeMe");
+		System.out.println("strMain is: \"" + strMain + "\" according to main after changeMe");
+		System.out.println("First number of the array is: " + arrMain[0] + " according to main after changeMe");
+		/*
+		 * num is: 7 according to main after changeMe
+		 * strMain is: "APCS" according to main after changeMe
+		 * First number of the array is: 6 according to main after changeMe
+		 */
 	}
 	
 	/**
-	 * Seems like variable types such as int's and String's 
-	 * seem to be unaffected by changing their values outside 
-	 * a main by a method in the end. However, Array's seem to
-	 * continue storing the changes made by the method.
+	 * arrMain[] changes after the method changeMe
+	 * because the reference of the value in arrMain[]
+	 * passes through main, while other variable types
+	 * (Strings, int, etc) do not. Also worth noting 
+	 * String is an object-like object that cannot store
+	 * values outside the method
 	 */
-	
 	public static void changeMe(int x, String str, int[] arr) {
-		System.out.println("Commence changing!");
-		System.out.println();
-	
-		//Beep-boop. Method changeMe has changed num. num is now 8. Beep boop.
+		//Part 1
+		System.out.println("Changing things.");
+		
 		x++;
-		System.out.println("Beep-boop. Method changeMe has changed num. num is now " + x + ". Beep boop.");
+		System.out.println("num is: " + x + " according to changeMe");
 		
-		//Beep-boop. Method changeMe has changed strMain. strMain is now "APCS is the best class!" Blip blop.
-		str += " is the best class!";
-		System.out.println("Beep-boop. Method changeMe has changed strMain. strMain is now \"" + str + "\" Blip blop.");
+		str += " period 2";
+		System.out.println("strMain is: \"" + str + "\" according to changeMe");
 		
-		//Beep-boop. Method changeMe has changed arrMain. The fifth number of arrMain is now 64. Ding dong.
-		arr[4] = 64;
-		System.out.println("Beep-boop. Method changeMe has changed arrMain. The fifth number of arrMain is now " + arr[4] + ". Ding dong.");
+		arr[0] = 6;
+		System.out.println("First number of the array is: " + arr[0] + " according to changeMe");
+		System.out.println();
+		
+		//Part 2
+		
+		/*
+		 * Expected output: "The value of bInt is: 3"
+		 * Actual output: "The value of bInt is: 3"
+		 */
+		x = 3;
+		int bInt = x;
+		x = 23;
+		System.out.println("The value of bInt is: " + bInt);
+
+		/*
+		 * Expected output: "hhhhhhh"
+		 * Actual output: "hhhhhhh"
+		 */
+		str = "hhhhhhh";
+		String bString = str;
+		str = "iiiiiii";
+		System.out.println("The value of bString is: \"" + bString + "\"");
+		
+		/*
+		 * Expected output: "The value of the first number of bArr is: 2"
+		 * Actual output: "The value of the first number of bArr is: 3"
+		 */
+		arr[0] = 2;
+		int[] bArr = arr;
+		arr[0] = 3;
+		System.out.println("The value of the first number of bArr is: " + bArr[0]);
+		
 		System.out.println();
 	}
 }
-
