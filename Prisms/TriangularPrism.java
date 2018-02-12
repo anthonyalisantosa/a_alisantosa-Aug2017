5fbf6fe8cdb3dcf35ba3d7a4ee39c86e59e9bff7
anthonyalisantosa <31085589+anthonyalisantosa@users.noreply.github.com>
2018-02-11 22:39:40 -0800

    suh bih

---
+++ Prisms/TriangularPrism.java
@@ -0,0 +1,34 @@
+/* Write this concrete (non-abstract) class called TriangularPrism.  
+ * It has 3 private fields (sideA, sideB, and sideC).
+ * It inherits its height from its superclass, Prism (because all prisms have a height).
+ * Provide a constructor and the methods required by its abstract superclass.  
+ * 
+ * Math Note:
+ * Perimeter refers to the perimeter of the base - the sum of all the sides.
+ * To find the area of a triangle given 3 side lengths, look up Heron's formula.  
+ * It uses halfp the perimeter.  (Why might you want to use 0.5 instead of 1/2?)
+ */
+
+
+public class TriangularPrism extends Prism
+{
+	private double sideA;
+	private double sideB;
+	private double sideC;
+	
+	public TriangularPrism(double A, double B, double C, double height) {
+		super(height);
+		sideA = A;
+		sideB = B;
+		sideC = C;
+	}
+	
+	public double calcBase() {
+		double halfp = (sideA + sideB + sideC) / 2.0;
+		return Math.sqrt(halfp * (halfp - sideA) * (halfp - sideB) * (halfp - sideC));
+	}
+	
+	public double calcPerimeter() {
+		return sideA + sideB + sideC;
+	}
+}
